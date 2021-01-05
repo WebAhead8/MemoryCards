2ddde510b1a3ba82dc179677c3576eff4b003bf7
mahmoud Diab <mhmoddiab2@gmail.com>
2021-01-05 16:52:02 +0200

    add level 1

---
+++ level1.js
@@ -0,0 +1,74 @@
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let lockBoard = false
+let firstCard, secondCard;
+
+
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
+   this.classList.add('flip');
+
+   if (!hasFlippedCard) {
+      //first click 
+      hasFlippedCard = true;
+      firstCard = this;
+
+      return;
+   } 
+   //second click
+   
+   secondCard = this;  
+       
+   checkForMatch();
+   
+}
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.img === secondCard.dataset.img;
+        
+    isMatch ? disableCards() : unflipCards();
+      
+}
+function disableCards() {
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
+
+    resetBoard()
+}
+
+function unflipCards() {
+    lockBoard = true;
+    setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');  
+       
+    resetBoard();
+  }, 1000)
+}
+
+
+function resetBoard() {  // to let you click the card you have clicked befor
+    hasFlippedCard = false;
+    lockBoard = false;
+    firstCard = null;
+    secondCard = null;
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
+})();
+
+
+
+
+
+
+
+
+cards.forEach(card => card.addEventListener('click', flipCard))
